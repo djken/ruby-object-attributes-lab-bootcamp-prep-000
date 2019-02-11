c13000aba9752db5d1ad624dd19c954b7e8ee08f
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-02-11 16:39:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,9 @@
 class Dog
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def name 
     @name
   end
 end
-
-metoo = Dog.new("Metoo")
-puts metoo.name
-
-metoo.name = "Mike"
-puts metoo.name
